b83fa7c6ed5c2afb04f98ddc90eb09225055aa26
David Byrne <X00203114@mytudublin.ie>
2023-11-27 16:27:06 +0000

    models working

--- shop/models.py
+++ shop/models.py
@@ -8,19 +8,19 @@ class Category(models.Model):
         default=uuid.uuid4,
         editable=False)
     name = models.CharField(max_length=250, unique=True)
-    description = models.TextField(blank = True)
-    image = models.ImageField(upload_to = 'category', blank=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='category', blank=True)
 
-class Meta:
-    ordering = ('name',)
-    verbose_name = 'category'
-    verbose_name_plural = 'categories'
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
-def get_absolute_url(self):
-    return reverse('shop:products_by_category', args=[self.id])
+    def get_absolute_url(self):
+        return reverse('shop:products_by_category', args=[self.id])
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     id = models.UUIDField(
@@ -28,14 +28,14 @@ class Product(models.Model):
         default=uuid.uuid4,
         editable=False)
     name = models.CharField(max_length=250, unique=True)
-    description = models.TextField(blank = True)
+    description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to = 'product', blank=True)
+    image = models.ImageField(upload_to='product', blank=True)
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True, blank = True, null= True)
-    updated = models.DateTimeField(auto_now=True, blank = True, null= True)
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     class Meta:
         ordering = ('name',)
