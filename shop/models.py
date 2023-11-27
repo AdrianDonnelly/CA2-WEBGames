2752d04996dc29c4b03a43a3c1d999318f0ab85e
David Byrne <X00203114@mytudublin.ie>
2023-11-27 13:37:01 +0000

    model layout

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,6 @@
-import uuid
 from django.db import models
-
-# Create your models here.
+import uuid
+from django.urls import reverse
 
 class Category(models.Model):
     id = models.UUIDField(
@@ -18,8 +17,33 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
 
     def get_absolute_url(self):
-        return reverse('shop:products_by_category', args =[self.id])
-        
-        
+        return reverse('shop:products_by_category', args=[self.id])
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
+    name = models.CharField(max_length=250, unique=True)
+    description = models.TextField(blank = True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to = 'product', blank=True)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, blank = True, null= True)
+    updated = models.DateTimeField(auto_now=True, blank = True, null= True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.category.id, self.id])
+
     def __str__(self):
         return self.name
