59369cc2b6da2c61290df4e3265cbe72a4501b26
David Byrne <X00203114@mytudublin.ie>
2023-11-30 13:33:53 +0000

    signup age restriction

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from django.core.exceptions import ValidationError
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('username','email','age',)
+    
+    def age_restriction(self):
+        age = self.cleaned_data.get('age')
+        if age is not None and age <18:
+            raise ValidationError(_('You must be at least 18 years old to register.'))
+        return age
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
--- accounts/views.py
+++ accounts/views.py
@@ -1,28 +1,29 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.models import Group
 from .forms import CustomUserCreationForm
 from .models import CustomUser
 
-
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
-    def post(self, request, *args, **kwargs):
-        form = CustomUserCreationForm(request.POST)
+    def form_valid(self, form):
         try:
             customer_group = Group.objects.get(name='Customer')
         except Group.DoesNotExist:
             customer_group = Group.objects.create(name='Customer')
-        if form.is_valid():
-            signup_user = form.save()
-            username = form.cleaned_data.get('username')
-            signup_user = CustomUser.objects.get(username=username)
-            customer_group = Group.objects.get(name='Customer')
-            customer_group.user_set.add(signup_user)
-            return redirect('login')
-        else:
-            return render(request, self.template_name, {'form' : form })
+
+        # Validate age if its above 18 to make account
+        age = form.cleaned_data.get('age')
+        if age is not None and age < 18:
+            form.add_error('age', 'You must be at least 18 years old to register.')
+            return self.form_invalid(form)
+
+        signup_user = form.save()
+        username = form.cleaned_data.get('username')
+        signup_user = CustomUser.objects.get(username=username)
+        customer_group.user_set.add(signup_user)
+        return super().form_valid(form)
